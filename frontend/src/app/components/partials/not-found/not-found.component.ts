277c39867a950de51884bb95479542bf4319d6bd
Gaurav <gauravtak2609@gmail.com>
2024-03-10 16:27:54 +0530

    Added notfound component

--- frontend/src/app/components/partials/not-found/not-found.component.ts
+++ frontend/src/app/components/partials/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent {
+
+  @Input()
+  visible = false;
+
+  @Input()
+  notFoundMessage = "Nothing Found!";
+
+  @Input()
+  resetLinkText = "Reset";
+
+  @Input()
+  resetLinkRoute = "/";
+  
+  constructor() { }
 
 }
